b399fcd79c87c890bc92a5facce77c33a84d8319
Sterling Baldwin <sterling16@mac.com>
2016-04-04 23:41:00 -0800

    Pivot is working, now for elimination

--- gauss.cpp
+++ gauss.cpp
@@ -7,7 +7,25 @@
 using namespace std;
 
 vector<double> gauss(vector<vector<double> > A){
-  //Do gauss
+  int n = A.size();
+
+  for (int i=0; i<n; i++) {
+      // Search for maximum in this column
+      double maxEl = abs(A[i][i]);
+      int maxRow = i;
+      for (int k=i+1; k<n; k++) {
+          //if the value is larger then max, set max to val
+          if (abs(A[k][i]) > maxEl) {
+              maxEl = abs(A[k][i]);
+              maxRow = k;
+          }
+      }
+  }
+
+  // Solve equation Ax=b for an upper triangular matrix A
+  vector<double> x(n);
+  // Do back substitution
+  return x;
 }
 
 int main(int argc, char *argv[]) {
