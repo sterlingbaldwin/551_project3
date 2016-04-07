63ef4990ec05476eb6753738608e603043c7cb17
Sterling Baldwin <sterling16@mac.com>
2016-04-07 14:35:00 -0800

    Cant get the stupid pointers to work, reverting back to stack eventhough its slow

--- gauss.cpp
+++ gauss.cpp
@@ -6,38 +6,35 @@
 #include <sstream>
 using namespace std;
 
-vector<double> gauss(vector<vector<double>* > A){
-  int n = A->size();
+vector<double> gauss(vector<vector<double> > A){
+  int n = A.size();
 
   for (int i=0; i<n; i++) {
       // Search for maximum in this column
-      double maxEl = fabs((*A[i][i]));
+      double maxEl = fabs(A[i][i]);
       int maxRow = i;
       for (int k=i+1; k<n; k++) {
           //if the value is larger then max, set max to val
-          if (fabs((*A[k][i])) > maxEl) {
-              maxEl = fabs((*A[k][i]));
+          if (fabs(A[k][i]) > maxEl) {
+              maxEl = fabs(A[k][i]);
               maxRow = k;
           }
       }
   }
 
-  // for (int k=i; k<n+1;k++) {
-  //     double tmp = A[maxRow][k];
-  //     A[maxRow][k] = A[i][k];
-  //     A[i][k] = tmp;
-  // }
-  vector<double>* tmp = A[maxRow];
-  A[maxRow] = A[i];
-  A[i] = tmp;
+  for (int k=i; k<n+1;k++) {
+      double tmp = A[maxRow][k];
+      A[maxRow][k] = A[i][k];
+      A[i][k] = tmp;
+  }
 
   for (int k=i+1; k<n; k++) {
-      double c = (*-A[k][i])/(*A[i][i]);
+      double c = -A[k][i]/A[i][i];
       for (int j=i; j<n+1; j++) {
           if (i==j) {
-              (*A[k][j]) = 0;
+              A[k][j] = 0;
           } else {
-              (*A[k][j]) += c * (*A[i][j]);
+              A[k][j] += c * A[i][j];
           }
       }
   }
@@ -45,9 +42,9 @@ vector<double> gauss(vector<vector<double>* > A){
   // Solve equation Ax=b for an upper triangular matrix A
   vector<double> x(n);
   for (int i=n-1; i>=0; i--) {
-      x[i] = (*A[i][n])/(*A[i][i]);
+      x[i] = A[i][n]/A[i][i];
       for (int k=i-1;k>=0; k--) {
-          (*A[k][n]) -= (*A[k][i]) * x[i];
+          A[k][n] -= A[k][i] * x[i];
       }
   }
   return x;
@@ -66,7 +63,7 @@ int main(int argc, char *argv[]) {
     cout << "theads should be:" << threads << "\n";
 
     vector<double> line(n+1,0);
-    vector< vector<double>* > A(n,line);
+    vector< vector<double> > A(n,line);
 
     srand48(time(NULL));
     for (int i=0; i<n; i++) {
@@ -93,9 +90,9 @@ int main(int argc, char *argv[]) {
     for(int i = 0; i<n; i++){
       double sum = 0;
       for(int j = 0; j<n; j++){
-          sum += (*A[i][j]) * x[j];
+          sum += A[i][j] * x[j];
       }
-      residual += pow(sum - (*A[i][n]), 2);
+      residual += pow(sum - A[i][n], 2);
     }
 
     cout << "residual: " << sqrt(residual) << "\n";
